27a80279a3d663a74e4da0e9ea238fe66f08198d
HarshRat <mail@harshrathore.me>
2020-07-19 12:37:47 +0530

    Initial

---
+++ users/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '12345678');
+define('DB_NAME', 'socialite');
+
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
---
+++ users/sign_in.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Socialite</title>
+    <link rel="stylesheet" href="/css/master.css">
+  </head>
+  <body>
+    <nav>
+      <div class="topnav">
+        <a href="/">Socialite</a>
+        <div class="topnav-right">
+          <a href="/users/sign_in.php" style="padding-left:0;">Login</a>
+          <a href="/users/sign_up.php" style="padding-left:0;">Register</a>
+        </div>
+      </div>
+    </nav>
+    <main>
+      <div class="container">
+        <div class="formholder">
+          <div class="heading">
+            Log in
+          </div>
+          <div class="form">
+            <div class="container">
+              <div class="form-heading">Email</div>
+              <input type="email" autofocus="autofocus" autocomplete="email" name="email" class="input-field">
+              <div class="form-heading">Password</div>
+              <input type="password" autocomplete="current-password" name="password" class="input-field">
+              <input type="submit" class="button" value="Log in">
+            </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
